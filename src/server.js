df363d7356dc9bb62ae60140681f73a954f70dcf
unknown <business.urfavteddybear@gmail.com>
2024-09-29 18:54:18 +0800

    chore: wrong dir && feat: advance logging

--- src/server.js
+++ src/server.js
@@ -2,21 +2,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
 dotenv.config();
 
-const bookRoutes = require('./src/routes/books');
-const categoryRoutes = require('./src/routes/categories');
+const bookRoutes = require('./routes/books');
+const categoryRoutes = require('./routes/categories');
 
 const app = express();
 
-// Logging middleware
-app.use((req, res, next) => {
-    const now = new Date();
-    const logMessage = `${now.toISOString()} - ${req.method} ${req.originalUrl}`;
-    console.log(logMessage);
-    next(); // Proceed to the next middleware or route handler
-});
+// Logging
+const accessLogStream = fs.createWriteStream(path.join(__dirname, '../access.log'), { flags: 'a' });
+app.use(morgan(':date[iso] - :remote-addr - :method :url :status :response-time ms', { stream: accessLogStream })); // Log to file
+app.use(morgan(':date[iso] - :remote-addr - :method :url :status :response-time ms')); // Log to console as well
+app.set('trust proxy', true);
 
 // Middlewares
 app.use(bodyParser.json());
